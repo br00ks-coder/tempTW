f754bff41c7090547d123f975b55f640d4a11b79
br00ks-coder <br00ks@protonmail.com>
2023-05-18 21:23:47 +0300

    hy

---
+++ loginFunc.php
@@ -0,0 +1,42 @@
+<?php	
+session_start();
+$dbconn = pg_connect("host=localhost dbname=webgardening user=postgres password=paroladb");
+if (!$dbconn) {
+    // Handle connection error
+    die("Connection failed: " . pg_last_error());
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];	
+    $password = $_POST['password'];
+   
+
+    $query = "SELECT id,username,password FROM users WHERE username = $1 AND password = $2";
+    $result = pg_query_params($dbconn, $query, array($username, $password));
+
+    if (!$result) {
+        // Handle query error
+        die("Query failed: " . pg_last_error());
+    }
+
+    $row = pg_fetch_assoc($result);
+    if ($row) {
+
+        // User exists, do further processing
+        $_SESSION['user_id'] = $row['id']; // Store user ID in session variable
+        $_SESSION['username'] = $row['username']; // Store username in session variable
+
+       // Redirect to a protected page or display a success message
+
+        header("Location: index.php");
+    exit; // Make sure to exit after the redirection
+    } else {
+        // User does not exist or credentials are incorrect
+        echo "Invalid username or password.";
+        header("Location: login.php");
+    }
+}
+
+pg_close($dbconn);
+?>
