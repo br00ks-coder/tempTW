e2bd60c9942af4a95915b43c3b6cce7394fd268d
Balan Alexandru <62931201+br00ks-coder@users.noreply.github.com>
2023-06-19 03:08:27 +0300

    Create delete_user.php

---
+++ delete_user.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+$dbconn = pg_connect("host=webgardeningrds.cepe7iq3kfqk.eu-north-1.rds.amazonaws.com port=5432 dbname=webgardening user=postgres password=paroladb");
+if (!$dbconn) {
+    // Handle connection error
+    die("Connection failed: " . pg_last_error());
+}
+
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['user_id'])) {
+        $userId = $_POST['user_id'];
+
+        // Delete the user from the database
+        $query = "DELETE FROM users WHERE id = $1";
+        $result = pg_query_params($dbconn, $query, array($userId));
+
+        if (!$result) {
+            // Handle query error
+            die("Query failed: " . pg_last_error());
+        }
+
+        echo "User deleted successfully.";
+    } else {
+        echo "Invalid request.";
+    }
+               header("Location: admin.php");
+
+
+
+pg_close($dbconn);
+?>
