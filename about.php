52942653fb3f5b047fbd6328fa23bd7cbaca7c48
Balan Alexandru <62931201+br00ks-coder@users.noreply.github.com>
2023-06-19 07:15:01 +0300

    Update about.php

--- about.php
+++ about.php
@@ -47,8 +47,11 @@ session_start();
 
 <div class="about-us">
   <h2>About Us</h2>
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac blandit eros. Curabitur suscipit leo ligula, vel posuere lorem malesuada sit amet. Nam vitae sapien auctor, lacinia tellus vel, bibendum mauris. Sed ac nisi mauris. Aliquam bibendum, metus a varius imperdiet, metus lectus elementum arcu, sit amet fermentum est sapien sit amet libero. Ut ac consectetur mauris, a accumsan elit. Pellentesque convallis, nulla at varius efficitur, est urna ultrices ex, sit amet rutrum quam eros vel arcu. Vivamus a lobortis justo, at tristique eros. Donec fermentum augue a felis faucibus, ut feugiat tellus hendrerit. Fusce posuere consequat nibh, in euismod dolor imperdiet nec. Aenean dapibus erat et magna suscipit laoreet.</p>
-  <p>Nullam mollis ipsum libero, eu tempus enim sollicitudin ut. Phasellus fermentum turpis in tortor malesuada, in bibendum quam dapibus. Ut ut velit odio. Maecenas a quam et mauris dapibus tincidunt. Nunc ut ex sed felis dignissim vehicula vel vel purus. Nunc malesuada varius luctus. Curabitur vestibulum elit at elit luctus eleifend. Ut blandit eleifend velit, sit amet consequat orci iaculis vitae. Integer convallis lacinia odio, eu tempor enim bibendum vel. Nam sagittis nisi nec arcu egestas venenatis. Suspendisse porttitor dolor vitae neque maximus, eu interdum magna euismod.</p>
+<p>
+  We are passionate about helping you create your very own green paradise right at home. Whether you're an experienced gardener or just starting out, our goal is to provide you with the knowledge, inspiration, and tools you need to nurture beautiful plants and create stunning outdoor spaces.
+
+With years of experience in the gardening industry, our team of experts is dedicated to sharing their expertise and helping you unleash your gardening potential. We understand that gardening is more than just a hobby—it's a way to connect with nature, find solace in the beauty of plants, and create a harmonious environment.
+</p>
 </div>
 <div class="flower-div" style="z-index: 2;" >
   
